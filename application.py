5412673f0f64b43ece04beb44c69de2a61c6cfec
Subin Babu <subin@telmate.com>
2018-06-27 23:57:42 -0700

    UI for creating and reading from the database

--- application.py
+++ application.py
@@ -7,10 +7,10 @@ Author: Scott Rodkey - rodkeyscott@gmail.com
 Step-by-step tutorial: https://medium.com/@rodkey/deploying-a-flask-application-on-aws-a72daba6bb80
 '''
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from application import db
-from application.models import Data
-from application.forms import EnterDBInfo, RetrieveDBInfo
+from application.models import Data, Juice
+from application.forms import RetrieveDBInfo,JuiceDBInfo
 
 # Elastic Beanstalk initalization
 application = Flask(__name__)
@@ -21,31 +21,30 @@ application.secret_key = 'cC1YCIWOj9GgWspgNEo2'
 @application.route('/', methods=['GET', 'POST'])
 @application.route('/index', methods=['GET', 'POST'])
 def index():
-    form1 = EnterDBInfo(request.form) 
-    form2 = RetrieveDBInfo(request.form) 
-    
-    if request.method == 'POST' and form1.validate():
-        data_entered = Data(notes=form1.dbNotes.data)
-        try:     
+    formjuice = JuiceDBInfo(request.form)
+
+    if request.method == 'POST' and formjuice.validate():
+        data_entered = Juice(juice_index=formjuice.dbJuiceIndex.data,juice_name=formjuice.dbJuiceName.data,nicotine=formjuice.dbJuiceNicotine.data,type=formjuice.dbJuiceType.data)
+        try:
             db.session.add(data_entered)
-            db.session.commit()        
+            db.session.commit()
             db.session.close()
         except:
             db.session.rollback()
-        return render_template('thanks.html', notes=form1.dbNotes.data)
-        
-    if request.method == 'POST' and form2.validate():
-        try:   
-            num_return = int(form2.numRetrieve.data)
-            query_db = Data.query.order_by(Data.id.desc()).limit(num_return)
-            for q in query_db:
-                print(q.notes)
-            db.session.close()
-        except:
-            db.session.rollback()
-        return render_template('results.html', results=query_db, num_return=num_return)                
-    
-    return render_template('index.html', form1=form1, form2=form2)
+        return render_template('thanks.html', juice=formjuice.dbJuiceName.data)
+
+    return render_template('index.html', formjuice = formjuice)
+
+@application.route('/juices', methods=['GET', 'POST'])
+def fetchJuices():
+    try:
+        query_db = Juice.query.order_by(Juice.juice_index.desc())
+        for q in query_db:
+            print(q.notes)
+        db.session.close()
+    except:
+        db.session.rollback()
+    return render_template('results.html', results=query_db)
 
 if __name__ == '__main__':
     application.run(host='0.0.0.0')
--- application/forms.py
+++ application/forms.py
@@ -1,8 +1,11 @@
 from flask.ext.wtf import Form
 from wtforms import TextField, validators
 
-class EnterDBInfo(Form):
-    dbNotes = TextField(label='Items to add to DB', description="db_enter", validators=[validators.required(), validators.Length(min=0, max=128, message=u'Enter 128 characters or less')])    
+class JuiceDBInfo(Form):
+    dbJuiceIndex = TextField(label='Index for the Juice', description="db_enter", validators=[validators.required(), validators.Regexp('^\d{1}$',message=u'Enter a number between 1 and 10')])
+    dbJuiceName = TextField(label='Name for the juice', description="db_enter", validators=[validators.required(), validators.Length(min=0, max=255, message=u'Enter 128 characters or less')])
+    dbJuiceNicotine = TextField(label='Nicotine Level of the juice', description="db_enter", validators=[validators.required(), validators.Regexp('^\d{1}$',message=u'Enter a number between 1 and 10')])
+    dbJuiceType = TextField(label='Type of the juice', description="db_enter", validators=[validators.required(), validators.Length(min=0, max=255, message=u'Enter 128 characters or less')])
 
 class RetrieveDBInfo(Form):
     numRetrieve = TextField(label='Number of DB Items to Get', description="db_get", validators=[validators.required(), validators.Regexp('^\d{1}$',message=u'Enter a number between 1 and 10')])
